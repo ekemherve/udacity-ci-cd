031c9be76cbd270a9b6ee0976240e00446f0a807
Ekem Herve <ekemherve@gmail.com>
2020-07-18 18:48:17 +0200

    All Endpoints work fine

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,11 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+		if(createUserRequest.getPassword().length() < 7 || !Objects.equals(createUserRequest.getConfirmPassword(),
+				createUserRequest.getConfirmPassword())){
+			return ResponseEntity.badRequest().build();
+		}
+		user.setPassword(createUserRequest.getPassword());
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
